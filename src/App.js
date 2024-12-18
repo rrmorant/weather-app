b1fce5f5a20f4ad8c68148cd929b6bb40b9dac5d
Ron M <duecel@icloud.com>
2024-12-17 23:03:33 -0800

    working

--- src/App.js
+++ src/App.js
@@ -5,6 +5,71 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFrown } from "@fortawesome/free-solid-svg-icons/faFrown";
 import "./App.css";
 
-function GfGWeatherApp() {}
+function GfGWeatherApp() {
+	const [input, setInput] = useState("");
+	const [weather, setWeather] = useState({
+		loading: false,
+		data: {},
+		error: false,
+	});
+
+	const toDataFunction = () => {
+		const months = [
+			"January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December",
+		];
+		const WeekDays = [
+			"Sunday",
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday",
+		];
+		const currentDate = new Date();
+		const date = `${WeekDays[currentDate.getDay()]} ${currentDate.getDate()} ${
+			months[currentDate.getMonth()]
+		}`;
+		return date;
+	};
+
+	const search = async (event) => {
+		if (event.key === "Enter") {
+			event.preventDefault();
+			setInput("");
+			setWeather({ ...weather, loading: true });
+			const url = "https://api.openweathermap.org/data/2.5/weather";
+			const api_key = "f00c38e0279b7bc85480c3fe775d518c";
+			await axios
+				.get(url, {
+					params: {
+						q: input,
+						units: "metric",
+						appid: api_key,
+					},
+				})
+				.then((res) => {
+					console.log("res", res);
+					setWeather({ data: res.data, loading: false, error: false });
+				})
+				.catch((error) => {
+					setWeather({ ...weather, data: {}, error: true });
+					setInput("");
+					console.log("error", error);
+				});
+		}
+	};
+}
 
 export default GfGWeatherApp;
