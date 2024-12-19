77de00b28ec0f99b0daaef336895e904bd164521
Ron M <duecel@icloud.com>
2024-12-18 20:20:35 -0800

    functional

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Oval } from "react-loader-spinner";
 import React, { useState } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFrown } from "@fortawesome/free-solid-svg-icons/faFrown";
+import { faFrown } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 
 function GfGWeatherApp() {
@@ -13,7 +13,7 @@ function GfGWeatherApp() {
 		error: false,
 	});
 
-	const toDataFunction = () => {
+	const toDateFunction = () => {
 		const months = [
 			"January",
 			"February",
@@ -70,6 +70,65 @@ function GfGWeatherApp() {
 				});
 		}
 	};
+
+	return (
+		<div className="App">
+			<h1 className="app-name">Weather App</h1>
+			<div className="search-bar">
+				<input
+					type="text"
+					className="city-search"
+					placeholder="Enter City Name.."
+					name="query"
+					value={input}
+					onChange={(event) => setInput(event.target.value)}
+					onKeyPress={search}
+				/>
+			</div>
+			{weather.loading && (
+				<>
+					<br />
+					<br />
+					<Oval type="Oval" color="black" height={100} width={100} />
+				</>
+			)}
+			{weather.error && (
+				<>
+					<br />
+					<br />
+					<span className="error-message">
+						<FontAwesomeIcon icon={faFrown} />
+						<span style={{ fontSize: "20px" }}>City not found</span>
+					</span>
+				</>
+			)}
+			{weather && weather.data && weather.data.main && (
+				<div>
+					<div className="city-name">
+						<h2>
+							{weather.data.name}, <span>{weather.data.sys.country}</span>
+						</h2>
+					</div>
+					<div className="date">
+						<span>{toDateFunction()}</span>
+					</div>
+					<div className="icon-temp">
+						<img
+							className=""
+							src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`}
+							alt={weather.data.weather[0].description}
+						/>
+						{Math.round(weather.data.main.temp)}
+						<sup className="deg">°C</sup>
+					</div>
+					<div className="des-wind">
+						<p>{weather.data.weather[0].description.toUpperCase()}</p>
+						<p>Wind Speed: {weather.data.wind.speed}m/s</p>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default GfGWeatherApp;
